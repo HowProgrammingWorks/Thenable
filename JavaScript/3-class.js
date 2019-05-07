06ffff2751ff1940a47218d05ece278950a30196
tshemsedinov <timur.shemsedinov@gmail.com>
2019-05-07 08:38:37 +0300

    Class example

---
+++ JavaScript/3-class.js
@@ -0,0 +1,26 @@
+'use strict';
+
+class Result {
+  then(onFulfilled) {
+    this.onFulfilled = onFulfilled;
+  }
+  ready(data) {
+    if (this.onFulfilled) this.onFulfilled(data);
+  }
+}
+
+class Security {
+  static getPerson(id) {
+    const res = new Result();
+    setTimeout(() => {
+      const person = { id, name: 'Marcus Aurelius' };
+      res.ready(person);
+    }, 1000);
+    return res;
+  }
+}
+
+(async () => {
+  const person = await Security.getPerson(10);
+  console.dir({ person });
+})();
