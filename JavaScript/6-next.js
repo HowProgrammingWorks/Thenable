5ba9ee7514b47dde9048f09baec0b8d8955a7fef
tshemsedinov <timur.shemsedinov@gmail.com>
2019-05-07 09:00:39 +0300

    Show difference between promise and thenable

---
+++ JavaScript/6-next.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const getNumbers = () => {
+  const numbers = [1, 2, 3];
+  return () => new Promise((resolve, reject) => {
+    const num = numbers.shift();
+    if (num) {
+      resolve(num);
+    } else {
+      reject(new Error('I heve no numbers for you'));
+    }
+  });
+};
+
+(async () => {
+  const getNext = getNumbers();
+  for (let i = 0; i < 5; i++) {
+    try {
+      const res = await getNext();
+      console.dir({ res });
+    } catch(err) {
+      console.dir({ err: err.message });
+    }
+  }
+})();
