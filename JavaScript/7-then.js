f4c8737217c5ac85476c40157c9333ed35e0ce6c
tshemsedinov <timur.shemsedinov@gmail.com>
2019-05-07 14:36:11 +0300

    Use then to get next value

---
+++ JavaScript/7-then.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const getNumbers = () => {
+  const numbers = [1, 2, 3];
+  return () => new Promise((resolve, reject) => {
+    const num = numbers.shift();
+    if (num) {
+      resolve(num);
+    } else {
+      reject(new Error('I heve no numbers for you'));
+    }
+  });
+};
+
+const getNext = getNumbers();
+for (let i = 0; i < 5; i++) {
+  getNext().then(
+    res => console.dir({ res }),
+    err => console.dir({ err: err.message })
+  );
+}
---
+++ JavaScript/8-chain.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const getNumbers = () => ({
+  numbers: [1, 2, 3],
+  then(onFulfilled, onRejected) {
+    const num = this.numbers.shift();
+    if (num) {
+      onFulfilled(num);
+    } else {
+      onRejected(new Error('I heve no numbers for you'));
+    }
+    return this; // chain thenable
+  }
+});
+
+const onSuccess = res => console.dir({ res });
+const onError = err => console.dir({ err: err.message });
+
+getNumbers()
+  .then(onSuccess, onError)
+  .then(onSuccess, onError)
+  .then(onSuccess, onError)
+  .then(onSuccess, onError)
+  .then(onSuccess, onError);
