2f81dc30cce89b0c99ded40b0851d1c1001ab70f
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-07-11 05:32:10 +0300

    Add query builder example

---
+++ JavaScript/a-query.js
@@ -0,0 +1,42 @@
+'use strict';
+
+class Query {
+  constructor(table) {
+    this.options = { table, fields: ['*'], where: {} };
+  }
+
+  where(conditions) {
+    Object.assign(this.options.where, conditions);
+    return this;
+  }
+
+  order(field) {
+    this.options.order = field;
+    return this;
+  }
+
+  limit(count) {
+    this.options.limit = count;
+    return this;
+  }
+
+  then(resolve) {
+    const { fields, where, limit, order } = this.options;
+    const cond = Object.entries(where).map((e) => e.join('=')).join(' AND ');
+    const sql = `SELECT ${fields} WHERE ${cond}`;
+    const opt = `ORDER BY ${order} LIMIT ${limit}`;
+    resolve(sql + ' ' + opt);
+  }
+}
+
+// Usage
+
+(async () => {
+
+  const sql = await new Query('cities')
+    .where({ country: 10, type: 1 })
+    .order('population')
+    .limit(10);
+  console.log(sql);
+
+})();
